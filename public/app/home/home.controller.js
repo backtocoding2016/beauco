9c8b61fd7b8058f7b8f3d8002724c6a3168dfe3b
cholical <ronding@live.unc.edu>
2017-04-12 02:28:12 -0400

    Added carousel to home page

--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -6,7 +6,9 @@
 	app = angular.module('imbeau');
 	app.controller('homeCtrl', ['$scope', '$rootScope', '$state', 'homeSvc', 'authSvc', function homeCtrl($scope, $rootScope, $state, homeSvc, authSvc){
 
-		FB.XFBML.parse();
+		//FB.XFBML.parse();
+
+		$scope.slides = [1, 2, 3, 4];
 
 	}]);
 }());
